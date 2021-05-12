89badc8b94acf8c2375bbfb01ebe281301a4c251
d532335 <d532335@gmail.com>
2021-05-12 18:49:08 -0400

    Styling for iPhone

--- src/app/massage/massage.component.ts
+++ src/app/massage/massage.component.ts
@@ -11,16 +11,16 @@ export class MassageComponent implements OnInit {
   serviceType: ServiceType;
 
   types: string[] = [
-    "<b>Swedish Massage</b><span>- A relaxing, gentle massage to nourish the body and relieve stress</span>",
-    "<a class='stm-hyperlinks' href='/maternity'>Table for Two Maternity Massage</a><span>- Relieves the discomforts of pregnancy, such as edema and lethargy, and prepares the body for an easier delivery and faster recovery</span>",
-    "<b>Custom Therapeutic Session</b><span>- Any combination of deep tissue, neuromuscular or <a class='stm-hyperlinks' href='/cupping'>cupping</a> designed to address muscle tension and restoring range of motion.</span>"
+    "<b>Swedish Massage</b>A relaxing, gentle massage to nourish the body and relieve stress",
+    "<a class='stm-hyperlinks' href='/maternity'>Table for Two Maternity Massage</a>- Relieves the discomforts of pregnancy, such as edema and lethargy, and prepares the body for an easier delivery and faster recovery",
+    "<b>Custom Therapeutic Session</b>- Any combination of deep tissue, neuromuscular or <a class='stm-hyperlinks' href='/cupping'>cupping</a> designed to address muscle tension and restoring range of motion."
   ];
 
   otherTypesHeader: string = "Other Massage & Bodywork Services";
   otherTypes: string[] = [
-    "<b>Headache Session: </b><span> A head and neck session that focuses on relaxing the tightened muscles that cause many tension-related headaches - 15-20 minutes $20</span>",
-    "<b>Sinus Massage: </b><span> Combat sinus pain and pressure without drugs! This sessions helps provide relief from congestion and swelling by massaging the face and neck in specific pressure points to open sinuses and allow for drainage - 15 minutes $20</span>",
-    "<b>Hands and Feet Session: </b><span> Pampers hands and feet with a combination of reflexology, stretching and stimulating massage techniques. Great for anyone who  may suffer from carpal tunnel, arthritis or other wrist and ankle problems - 20-25 minutes $25</span>"
+    "<b>Headache Session: </b>A head and neck session that focuses on relaxing the tightened muscles that cause many tension-related headaches - 15-20 minutes $20",
+    "<b>Sinus Massage: </b>Combat sinus pain and pressure without drugs! This sessions helps provide relief from congestion and swelling by massaging the face and neck in specific pressure points to open sinuses and allow for drainage - 15 minutes $20",
+    "<b>Hands and Feet Session: </b>Pampers hands and feet with a combination of reflexology, stretching and stimulating massage techniques. Great for anyone who  may suffer from carpal tunnel, arthritis or other wrist and ankle problems - 20-25 minutes $25"
   ];
 
   policies: string[] = [
